395a15327836a530f46bc51a0db032656ba93505
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-12 15:17:44 +0300

    drawing location preview view with respective location

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -22,6 +22,16 @@ struct LocationsView: View {
                 header
                     .padding()
                 Spacer()
+
+                ZStack {
+                    ForEach(vm.locations){ location in
+                        if vm.mapLocation == location {
+                        LocationPreviewView(location: location)
+                            .shadow(color: Color.black.opacity(0.3), radius: 20)
+                            .padding()
+                        }
+                    }
+                }
             }
         }
     }
