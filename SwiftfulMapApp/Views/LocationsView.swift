e27abaec37fd23dce7b7cea2fd499adfe01290c9
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-12 16:31:55 +0300

    refactor views

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -14,19 +14,7 @@ struct LocationsView: View {
 
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion,
-                annotationItems: vm.locations,
-                annotationContent: { location in
-                //MapMarker(coordinate: location.coordinates, tint: .blue)
-                MapAnnotation(coordinate: location.coordinates) {
-                    LocationMapAnotationView()
-                        .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
-                        .shadow(radius: 10)
-                        .onTapGesture {
-                            vm.showNextLocation(location: location)
-                        }
-                }
-            })
+            mapLayer
                 .ignoresSafeArea()
 
             VStack(spacing: 0) {
@@ -34,19 +22,7 @@ struct LocationsView: View {
                 header
                     .padding()
                 Spacer()
-
-                ZStack {
-                    ForEach(vm.locations){ location in
-                        if vm.mapLocation == location {
-                        LocationPreviewView(location: location)
-                            .shadow(color: Color.black.opacity(0.3), radius: 20)
-                            .padding()
-                            .transition(.asymmetric(
-                                insertion: .move(edge: .trailing),
-                                removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+                locationPreviewStack
             }
         }
     }
@@ -89,5 +65,36 @@ extension LocationsView {
         .background(.thinMaterial)
         .cornerRadius(10)
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+            //MapMarker(coordinate: location.coordinates, tint: .blue)
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+
+    private var locationPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations){ location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
