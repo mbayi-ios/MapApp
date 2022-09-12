5baf85398776ff5aa24b2322734f2b496826ffab
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-12 12:52:43 +0300

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Amby on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
