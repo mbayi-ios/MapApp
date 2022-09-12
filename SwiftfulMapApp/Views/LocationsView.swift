796dc0794662b98bf7f780e87876a674bc5f16a1
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-12 14:00:37 +0300

    created header to display location

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -16,6 +16,13 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+
+            VStack(spacing: 0) {
+
+                header
+                .padding()
+                Spacer()
+            }
         }
     }
 }
@@ -24,5 +31,21 @@ struct LocationsView_Previews: PreviewProvider {
     static var previews: some View {
         LocationsView()
             .environmentObject(LocationsViewModel())
+    }
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Text(vm.mapLocation.name + ", " +  vm.mapLocation.cityName)
+                .font(.title2)
+                .fontWeight(.black)
+                .foregroundColor(.primary)
+                .frame(height: 55)
+            .frame(maxWidth: .infinity)
+        }
+        .background(.thinMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
